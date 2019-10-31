1dd2a614f8ac6342f4b5e6c75e0c3a506ed80526
Ismail Lambat <lambat_15@hotmail.com>
2019-10-31 09:43:13 +0000

    Removed unused page objects

--- SFA.DFC.TestAutomation/SFA.DFC.SkillsAssessment.UITests/Project/Tests/Pages/SHCHomePage.cs
+++ SFA.DFC.TestAutomation/SFA.DFC.SkillsAssessment.UITests/Project/Tests/Pages/SHCHomePage.cs
@@ -8,16 +8,11 @@ namespace SFA.DFC.SkillsAssessment.UITests.Project.Tests.Pages
     public class SHCHomePage : BasePage
     {
         #region Helpers
-        private readonly PageInteractionHelper _pageHelper;
         private readonly FormCompletionHelper _formHelper;
         private readonly ScenarioContext _context;
-        private readonly ObjectContext _objectContext;
-        private readonly IWebDriver _webDriver;
-        private readonly SkillsAssessmentConfig _config;
         #endregion
         #region Page Elements
         protected override string PageTitle => "";
-        private By AssessType = By.CssSelector("");
         
         private By StartSkillsHealthCheck = By.CssSelector(".button");
 
@@ -25,11 +20,7 @@ namespace SFA.DFC.SkillsAssessment.UITests.Project.Tests.Pages
         public SHCHomePage(ScenarioContext context) : base(context)
         {
             _context = context;
-            _pageHelper = context.Get<PageInteractionHelper>();
             _formHelper = context.Get<FormCompletionHelper>();
-            _objectContext = context.Get<ObjectContext>();
-            _webDriver = context.GetWebDriver();
-            _config = context.GetSkillsAssessmentConfig<SkillsAssessmentConfig>();
         }
         public YourAssessmentsPage ClickNewHealthCheck()
         {
