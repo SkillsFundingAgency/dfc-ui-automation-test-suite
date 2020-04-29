64eb19147a98e7957a466058d0f9ef29dd7bdc34
James Barnes <james@testtrends.co.uk>
2020-04-29 13:21:34 +0100

    Added project to solution.

---
+++ SFA.DFC.TestAutomation/SFA.DFC.CompositeApps.UITests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SFA.DFC.CompositeApps.UITests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
