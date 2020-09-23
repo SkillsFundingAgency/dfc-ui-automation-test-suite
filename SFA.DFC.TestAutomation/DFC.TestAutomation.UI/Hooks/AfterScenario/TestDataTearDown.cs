fe17d70a181dec94d11ebe0a083d1a8f182c0edc
James Barnes <james@testtrends.co.uk>
2020-09-23 13:07:00 +0100

    Story/DFCC-427 update nuget packages (#62)
    * Updated chrome driver
    
    * Part way through updating the packages. Build succeeding
    
    * More packages successfully updated. Specflow remaining
    
    * Updated Specflow

--- SFA.DFC.TestAutomation/DFC.TestAutomation.UI/Hooks/AfterScenario/TestDataTearDown.cs
+++ SFA.DFC.TestAutomation/DFC.TestAutomation.UI/Hooks/AfterScenario/TestDataTearDown.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using TechTalk.SpecFlow;
+using System.Globalization;
 
 namespace DFC.TestAutomation.UI.Hooks.AfterScenario
 {
@@ -46,7 +47,7 @@ namespace DFC.TestAutomation.UI.Hooks.AfterScenario
 
                 using (var writer = new StreamWriter(filePath))
                 {
-                    using (var csv = new CsvWriter(writer))
+                    using (var csv = new CsvWriter((TextWriter)writer, CultureInfo.CurrentCulture))
                     {
                         csv.WriteRecords(records);
                         writer?.Flush();
